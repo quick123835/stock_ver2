48f8ca609921c0620fd8c3766552c42e5a0b7f56
ray.chen <ray.chen@funpodium.net>
2024-02-01 15:55:15 +0800

    fixed deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: false,
+  reactStrictMode: true,
   swcMinify: true,
-  assetPrefix: '/stock_ver2/',
-  basePath: '/stock_ver2',
 }
 
 module.exports = nextConfig
